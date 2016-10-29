078291be4af27790db2c51ad49fa7b9bb4a1e55f
zhaotoday <729234283@qq.com>
2016-10-30 01:01:18 +0800

    update

--- src/app/dashboard/components/body/index.js
+++ src/app/dashboard/components/body/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './theme/styles'
-import ad1 from './theme/images/ad-1.png'
-import ad2 from './theme/images/ad-2.png'
-import ad3 from './theme/images/ad-3.png'
-import ad4 from './theme/images/ad-4.png'
-import ReactSwipe from 'react-swipe';
+import ad1 from './theme/images/ad-1.jpg'
+import ad2 from './theme/images/ad-2.jpg'
+import ad3 from './theme/images/ad-3.jpg'
+import ad4 from './theme/images/ad-4.jpg'
+import ReactSwipe from 'react-swipe'
 
 export default class extends React.Component {
   render() {
