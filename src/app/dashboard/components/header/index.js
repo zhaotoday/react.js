d979e6772fdab08aa8e4f4a560ef34c1dbcc1cc7
zhaotoday <729234283@qq.com>
2016-10-30 19:27:15 +0800

    update

--- src/app/dashboard/components/header/index.js
+++ src/app/dashboard/components/header/index.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import styles from './theme/styles'
+import Hammer from 'react-hammerjs'
 
 export default class extends React.Component {
   render() {
     return <header className={styles.header}>
       <div className={styles.overlay}>
         <h1 className="f1">
-          <div className={styles.left}></div>
-          请求大师
-          <div className={styles.right}></div>
+          <Hammer onTap={this._handleTapLeft}>
+            <div className={styles.left}></div>
+          </Hammer>
+          赢球大师
+          <Hammer onTap={this._handleTapRight}>
+            <div className={styles.right}></div>
+          </Hammer>
         </h1>
       </div>
       <div className={styles.logo}></div>
-      <div className={styles.download}></div>
+      <a className={styles.download} href="#"></a>
     </header>
+  }
+
+  _handleTapLeft() {
+    alert('left')
+  }
+
+  _handleTapRight() {
+    alert('right')
   }
 }
