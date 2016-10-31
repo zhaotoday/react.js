9bd70c7f3214ca3a9c1907ce0c18e1c36548799b
zhaotoday <729234283@qq.com>
2016-10-31 23:33:54 +0800

    update

--- src/app/dashboard/components/footer/index.js
+++ src/app/dashboard/components/footer/index.js
@@ -4,8 +4,7 @@ import styles from './theme/styles'
 export default class extends React.Component {
   render() {
     return <footer className={styles.footer}>
-      <div className={styles.icp}>福建虎翼网络科技有限公司 | 闽ICP备16028144号-1</div>
-      <div className={styles.logo}><p /></div>
+      福建虎翼网络科技有限公司 | 闽ICP备16028144号-1
     </footer>
   }
 }
