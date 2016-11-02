2e65706e01ce25bc67b4e5f7c094a8285b54dea6
820514 <820514@nd>
2016-11-02 18:07:09 +0800

    update

--- src/app/dashboard/components/footer/index.js
+++ src/app/dashboard/components/footer/index.js
@@ -4,7 +4,7 @@ import styles from './theme/styles'
 export default class extends React.Component {
   render() {
     return <footer className={styles.footer}>
-      福建虎翼网络科技有限公司 | 闽ICP备16028144号-1
+      福建虎翼网络科技有限公司 | 闽ICP备16028144号-1 | 客服QQ：289171496
     </footer>
   }
 }
--- src/app/dashboard/components/header/index.js
+++ src/app/dashboard/components/header/index.js
@@ -6,7 +6,7 @@ export default class extends React.Component {
   render() {
     return <header className={styles.header}>
       <div className={styles.logo} />
-      <a className={styles.download} href="#" />
+      <a className={styles.download} href="http://m.yingqiu8.com/download.html" />
     </header>
   }
 
