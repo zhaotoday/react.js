e12ef2536f2ca993202e0bb3e69ff807d2f38152
820514 <820514@nd>
2016-12-16 18:19:16 +0800

    update

--- src/utils/time.js
+++ src/utils/time.js
@@ -5,17 +5,37 @@ const DATE_FORMATTER = 'YYYY-MM-DD'
 const TIME_FORMATTER = 'HH:mm:ss'
 
 export default {
-  formatDate (dateTimeStr, formatter) {
-    if (!dateTimeStr) return ''
-    return dateTimeStr.date(formatter)
+  /**
+   * 格式化时间
+   * @param dateTime {string} 时间
+   * @param formatter {string} 格式
+   */
+  format(dateTime, formatter) {
+    if (!dateTime) return ''
+    return dateTime.date(formatter)
   },
-  getDateTime (dateTimeStr) {
-    return this.formatDate(dateTimeStr, DATE_TIME_FORMATTER)
+
+  /**
+   * 获取：年-月-日 时:分:秒
+   * @param dateTime {string} 时间
+   */
+  getDateTime(dateTime) {
+    return this.format(dateTime, DATE_TIME_FORMATTER)
   },
-  getDate (dateTimeStr) {
-    return this.formatDate(dateTimeStr, DATE_FORMATTER)
+
+  /**
+   * 获取：年-月-日
+   * @param dateTime {string} 时间
+   */
+  getDate(dateTime) {
+    return this.format(dateTime, DATE_FORMATTER)
   },
-  getTime (dateTimeStr) {
-    return this.formatDate(dateTimeStr, TIME_FORMATTER)
+
+  /**
+   * 获取：时:分:秒
+   * @param dateTime {string} 时间
+   */
+  getTime(dateTime) {
+    return this.format(dateTime, TIME_FORMATTER)
   }
 }
