3ee5f0e5bb7ee5a06af86791ab21b2d3008d435e
820514 <820514@nd>
2016-12-19 16:08:08 +0800

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { auth } from 'utils'
+import auth from 'utils/auth'
 
 module.exports = {
   component: 'div',
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-import storage from './storage'
+import storage from './helpers/storage'
 
 const TOKEN = 'TOKEN'
 const USER = 'USER'
