d2041b818ad892b63c12340b129be8f85000f0ee
820514 <820514@nd>
2017-02-04 17:14:26 +0800

    update

--- src/app/dashboard/index.js
+++ src/app/dashboard/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import styles from './theme/styles'
 
 export default class extends React.Component {
   render() {
-    return <div>dashboard</div>
+    return <div>
+      <div className={styles.nd} />
+      dashboard
+    </div>
   }
 }
