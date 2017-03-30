b4825450c4a18a307c4fcc5c4eddedb17ffd9f45
zhaotoday <729234283@qq.com>
2017-03-30 23:57:31 +0800

    update

--- src/components/panel/index.js
+++ src/components/panel/index.js
@@ -4,16 +4,6 @@ import Head from './components/head'
 import Body from './components/body'
 
 const Comp = class extends React.Component {
-  static propTypes = {
-    value: React.PropTypes.string
-  }
-
-  static defaultProps = {
-    value: ''
-  }
-
-  state = {}
-
   render() {
     return <div className={styles.panel}>
       {this.props.children}
